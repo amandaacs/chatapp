be581aa7d68579fbddc92472ea3fb75256cafbc7
amandaacs <amanda_cs@aol.com>
2024-11-02 13:36:58 -0300

    fixed socketcontext

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
     const {authUser} = useAuthContext();
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://chatapp-back-f1ei.onrender.com", {
                 query: {
                     userId: authUser._id
                 }
